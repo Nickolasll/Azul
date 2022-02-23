2766e853a8a2cf7f3756676c0b902eedbe3ceeeb
NickolasII <NickolasII@yanex.ru>
2022-02-23 16:43:00 +0300

    Bag prototype

--- Assets/Bag/Bag.cs
+++ Assets/Bag/Bag.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Bag : MonoBehaviour
 {
+    private List<string> tileTypes = new List<string> {
+        "Red",
+        "Blue",
+        "Yellow",
+        "Black",
+        "Diamond",
+    };
+    private int tileCount = 20;  // count of tiles of one color in the bag
+    private List<GameObject> tiles = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tileTypes.ForEach(type => {
+            for (int i = 0; i < this.tileCount; i++) {
+                
+            };
+        });
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Shuffle()
+    {
+
     }
 }
